a440ecde8915351a40abfcf46ac895dd93c5ac3d
Michele Grieco <m.grieco31@studenti.uniba.it>
2025-06-24 11:18:58 +0200

    typo fix

--- app/routes.py
+++ app/routes.py
@@ -74,7 +74,7 @@ def register():
 def user(username):
     user = db.first_or_404(sa.select(User).where(User.username == username))
     posts = [
-        {'author': user, 'body': 'Test post #1'}
+        {'author': user, 'body': 'Test post #1'},
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
