d8c0a651544e559d72e2e61180506237e17add8d
Michele Grieco <michelegrieco92@gmail.com>
2025-06-26 11:59:15 +0200

    user information logic and template

--- app/models.py
+++ app/models.py
@@ -14,6 +14,8 @@ class User(UserMixin, db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256)) # 'Optional' attribute for NULL values
     posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author') # WriteOnlyMapped defines posts as a collection type with Post objects inside
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
     
     def avatar(self, size):
         """
