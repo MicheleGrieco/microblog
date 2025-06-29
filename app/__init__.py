fb03ae26843045bdbb4ac2131a1231b48dca07ed
Michele Grieco <michelegrieco92@gmail.com>
2025-06-29 18:45:15 +0200

    file logger

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 import logging
-from logging.handlers import SMTPHandler
+import os
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 from flask import Flask
 from config import Config
@@ -31,5 +32,19 @@ if not app.debug:
         )
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+        
+        # File logging
+        # Categories: DEBUG, INFO, WARNING, ERROR, CRITICAL
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+        # Custom formatting
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+        ))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Microblog startup')
 
 from app import routes, models, errors
