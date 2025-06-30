8c47816eee7b42e1d5e84406d35e7d79ead9642a
Michele Grieco <michelegrieco92@gmail.com>
2025-06-30 11:04:11 +0200

    comment about Migrate module

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_migrate import Migrate # Flask wrapper for Alembic, a database migration framework for SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
