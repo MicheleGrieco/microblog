47dca79045a6d6d84d3a2b269c939fae52dabaa7
Michele Grieco <michelegrieco92@gmail.com>
2025-07-06 09:12:57 +0200

    pagination global variable

--- config.py
+++ config.py
@@ -24,3 +24,4 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['michelegrieco92@gmail.com']
+    POSTS_PER_PAGE = 3
