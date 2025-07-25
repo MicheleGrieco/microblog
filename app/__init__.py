6282c64f4eb9269b2b44b7ef35287fed08d97c51
Michele Grieco <m.grieco31@studenti.uniba.it>
2025-07-25 18:14:37 +0200

    babel object, get_locale function

--- app/__init__.py
+++ app/__init__.py
@@ -16,13 +16,17 @@ import logging
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate # Flask wrapper for Alembic, a database migration framework for SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
+from flask_babel import Babel
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -31,6 +35,7 @@ migrate = Migrate(app, db) # Database migration engine
 login = LoginManager(app)
 mail = Mail(app)
 moment = Moment(app)
+babel = Babel(app, locale_selector=get_locale)
 
 # Endpoint for login view
 login.login_view = 'login' # type: ignore
