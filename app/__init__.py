391a0878997fd8dc9ad5d6d8bdb99ae5f24bc3f8
Michele Grieco <michelegrieco92@gmail.com>
2025-07-29 17:26:36 +0200

    italian and spanish translations

--- app/__init__.py
+++ app/__init__.py
@@ -26,7 +26,8 @@ from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
 
 def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    # return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return 'es'
 
 app = Flask(__name__)
 app.config.from_object(Config)
