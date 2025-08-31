117f223c23656a882238e99971ad586fd51a2577
Michele Grieco <michelegrieco92@gmail.com>
2025-08-31 10:52:11 +0200

    module description

--- app/cli.py
+++ app/cli.py
@@ -1,3 +1,19 @@
+"""
+    Translation and localization commands for the Flask application.
+    These commands use the Flask CLI integration to provide a set of commands
+    for managing translations using the Babel toolchain.
+
+    Replacing app with current_app does not work in this case because these commands
+    are registered at startup, not during the handling of a request, which is the only time
+    when current_app can be used. To remove the reference to app in this module,
+    I created another blueprint.
+
+    Flask puts commands that are attached to blueprints under a group with the blueprint's name by default.
+    That would have caused these commands to be available as flask cli translate <command>.
+    To aboid the extra cli group, the cli_group=None argument is given to the blueprint,
+    then I register this cli blueprint in the application factory function in app/__init__.py.
+"""
+
 import os
 from flask import Blueprint
 import click
