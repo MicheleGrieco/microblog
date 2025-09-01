064263eafea939688f88a48339b66801014bfb03
Michele Grieco <michelegrieco92@gmail.com>
2025-09-01 16:34:14 +0200

    search form with elasticsearch

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import ValidationError, DataRequired, Length
 import sqlalchemy as sa
+from flask import request
 from flask_babel import _, lazy_gettext as _l
 from app import db
 from app.models import User
@@ -33,3 +34,14 @@ class PostForm(FlaskForm):
     post = TextAreaField(_l('Say something'), validators=[
         DataRequired(), Length(min=1, max=140)])
     submit = SubmitField(_l('Submit'))
+
+
+class SearchForm(FlaskForm):
+    q = StringField(_l('Search'), validators=[DataRequired()])
+    
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['form data'] = request.args
+        if 'meta' not in kwargs:
+            kwargs['meta'] = {'csrf': False}
+        super(SearchForm, self).__init__(*args, **kwargs)
