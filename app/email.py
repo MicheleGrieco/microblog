bf95bfeea83a977b79ee42cd5e1caa67b1a2ae7b
Michele Grieco <michelegrieco92@gmail.com>
2025-09-03 10:38:20 +0200

    return types for clarity

--- app/email.py
+++ app/email.py
@@ -21,12 +21,12 @@ from flask_mail import Message
 from app import mail
 
 
-def send_async_email(app, msg):
+def send_async_email(app, msg) -> None:
     with app.app_context():
         mail.send(msg)
 
 
-def send_email(subject, sender, recipients, text_body, html_body):
+def send_email(subject, sender, recipients, text_body, html_body) -> None:
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
