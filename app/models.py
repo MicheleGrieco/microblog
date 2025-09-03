0deca97d8c521b3e51f9600d6f69b95ff9c67a7e
Michele Grieco <michelegrieco92@gmail.com>
2025-09-03 10:43:13 +0200

    return types for clarity

--- app/models.py
+++ app/models.py
@@ -62,6 +62,7 @@ followers = sa.Table(
     sa.Column('followed_id', sa.Integer, sa.ForeignKey('user.id'),
               primary_key=True)
 )
+
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
@@ -84,37 +85,37 @@ class User(UserMixin, db.Model):
         secondaryjoin=(followers.c.follower_id == id),
         back_populates='following')
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return '<User {}>'.format(self.username)
 
-    def set_password(self, password):
+    def set_password(self, password) -> None:
         self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
+    def check_password(self, password) -> bool:
         return check_password_hash(self.password_hash, password) # type: ignore
 
-    def avatar(self, size):
+    def avatar(self, size) -> str:
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
-    def follow(self, user):
+    def follow(self, user) -> None:
         if not self.is_following(user):
             self.following.add(user)
 
-    def unfollow(self, user):
+    def unfollow(self, user) -> None:
         if self.is_following(user):
             self.following.remove(user)
 
-    def is_following(self, user):
+    def is_following(self, user) -> bool:
         query = self.following.select().where(User.id == user.id)
         return db.session.scalar(query) is not None
 
-    def followers_count(self):
+    def followers_count(self) -> Optional[int]:
         query = sa.select(sa.func.count()).select_from(
             self.followers.select().subquery())
         return db.session.scalar(query)
 
-    def following_count(self):
+    def following_count(self) -> Optional[int]:
         query = sa.select(sa.func.count()).select_from(
             self.following.select().subquery())
         return db.session.scalar(query)
@@ -134,13 +135,13 @@ class User(UserMixin, db.Model):
             .order_by(Post.timestamp.desc())
         )
 
-    def get_reset_password_token(self, expires_in=600):
+    def get_reset_password_token(self, expires_in=600) -> str:
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
             current_app.config['SECRET_KEY'], algorithm='HS256')
 
     @staticmethod
-    def verify_reset_password_token(token):
+    def verify_reset_password_token(token) -> Optional['User']:
         try:
             id = jwt.decode(token, current_app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
@@ -150,7 +151,7 @@ class User(UserMixin, db.Model):
 
 
 @login.user_loader
-def load_user(id):
+def load_user(id) -> Optional[User]:
     return db.session.get(User, int(id))
 
 
@@ -166,5 +167,5 @@ class Post(SearchableMixin, db.Model):
 
     author: so.Mapped[User] = so.relationship(back_populates='posts')
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return '<Post {}>'.format(self.body)
