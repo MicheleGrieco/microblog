742238d82ef1743f0cf6bdf335c28ff37a91be09
Michele Grieco <m.grieco31@studenti.uniba.it>
2025-09-04 10:32:41 +0200

    module description and functions docs

--- app/email.py
+++ app/email.py
@@ -1,4 +1,7 @@
 """
+Module name: email.py
+Author: Michele Grieco
+Description:
     Email sending functions.
     In the send_email() function, the application instance is passed as an argument
     to a background thread that will then deliver the email without blocking the main application.
@@ -13,6 +16,9 @@
     and pass that as the app argument.
     The current_app._get_current_object() expression extracts the actual application instance
     from inside the proxy object, so that is what I passed to the thread as an argument.
+Usage:
+    - send_email(subject, sender, recipients, text_body, html_body): Send an email asynchronously.
+    - send_async_email(app, msg): Helper function to send email in a background thread.
 """
 
 from threading import Thread
@@ -22,11 +28,26 @@ from app import mail
 
 
 def send_async_email(app, msg) -> None:
+    """
+    Send an email in a background thread.
+    :param app: The Flask application instance.
+    :param msg: The email message to send.
+    :return: None
+    """
     with app.app_context():
         mail.send(msg)
 
 
 def send_email(subject, sender, recipients, text_body, html_body) -> None:
+    """
+    Send an email asynchronously.
+    :param subject: The subject of the email.
+    :param sender: The sender's email address.
+    :param recipients: A list of recipient email addresses.
+    :param text_body: The plain text body of the email.
+    :param html_body: The HTML body of the email.
+    :return: None
+    """
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
