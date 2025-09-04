0acf67471933081ac85373e0c342ccd579227585
Michele Grieco <m.grieco31@studenti.uniba.it>
2025-09-04 10:24:23 +0200

    model description and functions docs

--- app/search.py
+++ app/search.py
@@ -1,7 +1,26 @@
 # type: ignore
+
+"""
+    Module name: search.py
+    Author: Michele Grieco
+    Description:
+        Elasticsearch integration for indexing and searching model instances.
+    Usage:
+        - add_to_index(index, model): Add a model instance to the Elasticsearch index.
+        - remove_from_index(index, model): Remove a model instance from the Elasticsearch index.
+        - query_index(index, query, page, per_page): Query the Elasticsearch index for a given search term.
+"""
+
 from flask import current_app
 
 def add_to_index(index, model) -> None:
+    """
+    Add a model instance to the Elasticsearch index.
+    The model must have a __searchable__ attribute listing the fields to index.
+    :param index: The name of the Elasticsearch index.
+    :param model: The model instance to index.
+    :return: None
+    """
     if not current_app.elasticsearch:
         return
     payload = {}
@@ -11,12 +30,26 @@ def add_to_index(index, model) -> None:
 
 
 def remove_from_index(index, model) -> None:
+    """
+    Remove a model instance from the Elasticsearch index.
+    :param index: The name of the Elasticsearch index.
+    :param model: The model instance to remove from the index.
+    :return: None
+    """
     if not current_app.elasticsearch:
         return
     current_app.elasticsearch.delete(index=index, id=model.id)
 
 
 def query_index(index, query, page, per_page) -> tuple[list[int], int]:
+    """
+    Query the Elasticsearch index for a given search term.
+    :param index: The name of the Elasticsearch index.
+    :param query: The search term.
+    :param page: The page number for pagination.
+    :param per_page: The number of results per page.
+    :return: A tuple containing a list of matching record IDs and the total number of matches
+    """
     if not current_app.elasticsearch:
         return [], 0
     search = current_app.elasticsearch.search(
