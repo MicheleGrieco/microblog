78aae4d44bc7456527388f65f8e9b1c70841cc40
Michele Grieco <michelegrieco92@gmail.com>
2025-09-05 11:37:02 +0200

    module description

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,3 +1,11 @@
+"""
+Module name: auth.py
+Author: Michele Grieco
+Description:
+    This module initializes the authentication blueprint for the Flask application.
+    It sets up the blueprint and imports the routes associated with authentication.
+"""
+
 from flask import Blueprint
 
 bp = Blueprint('auth', __name__)
