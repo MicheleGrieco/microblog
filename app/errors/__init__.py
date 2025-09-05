babd78e847c1d1ef3fe464e48ed89dcb5c92f93c
Michele Grieco <michelegrieco92@gmail.com>
2025-09-05 10:51:30 +0200

    module description

--- app/errors/__init__.py
+++ app/errors/__init__.py
@@ -1,3 +1,11 @@
+"""
+Module name: errors.py
+Author: Michele Grieco
+Description:
+    This module initializes the error handling blueprint for the Flask application.
+    It sets up a Blueprint named 'errors' and imports the error handlers from the handlers module.
+"""
+
 from flask import Blueprint
 
 bp = Blueprint('errors', __name__)
