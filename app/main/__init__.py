49248742c23c2fa75ef4c15b45ac3bc7cd085509
Michele Grieco <michelegrieco92@gmail.com>
2025-09-05 10:56:44 +0200

    module description

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,3 +1,11 @@
+"""
+Module name: __init__.py
+Author: Michele Grieco
+Description:
+    This module initializes the 'main' Blueprint for the Flask application.
+    It sets up the Blueprint and imports the routes defined in the 'routes' module.
+"""
+
 from flask import Blueprint
 
 bp = Blueprint('main', __name__)
